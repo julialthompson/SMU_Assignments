99dba2ce0179497c9d15c712f6dce8f04d84ffab
Julia Thompson <julia.thompson92@gmail.com>
2019-12-15 13:18:59 -0600

    added plotly hw

---
+++ Unit_15_Plotly/Belly_Button_Biodiversity/static/js/bonus.js
@@ -0,0 +1,29 @@
+
+// //   make gauge chart
+//   var data = [
+//     {
+//       domain: { x: [0, 1], y: [0, 1] },
+//       value: 270,
+//     //   title: { text: "Speed" },
+//       type: "indicator",
+//       mode: "gauge+number",
+//       Gauge: {
+//         axis: { range: [null, 10] },
+//         steps: [
+//           { range: [0, 1], color: "lightgray" },
+//           { range: [1, 2], color: "lightgray" },
+//           { range: [2, 3], color: "lightgray" },
+//           { range: [3, 4], color: "lightgray" },
+//           { range: [4, 5], color: "lightgray" },
+//           { range: [5, 6], color: "lightgray" },
+//           { range: [6, 7], color: "lightgray" },
+//           { range: [7, 8], color: "lightgray" },
+//           { range: [8, 9], color: "gray" },
+//           { range: [9, 10], color: "darkgray" },
+//         ],
+//       }
+//     }
+//   ];
+  
+//   var layout = { width: 400, height: 400, margin: { t: 0, b: 0 } };
+//   Plotly.newPlot('gauge', data, layout);
